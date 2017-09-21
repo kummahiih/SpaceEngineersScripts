0719e0a36b3aba16bf3cc58f900f94b5cfac8514
pauli <pauli.rikula@gmail.com>
2017-09-22 00:13:51 +0300

    added group actions

--- IdleLoop.cs
+++ IdleLoop.cs
@@ -11,63 +11,58 @@ namespace IdleLoop
 {
     class Program : MyGridProgram
     {
-        #region copymeto programmable block 
-        //see https://github.com/kummahiih/SpaceEngineersScripts/blob/master/ .. somewhere 
+        #region copymeto programmable block    
+        //see https://github.com/kummahiih/SpaceEngineersScripts/blob/master/ .. somewhere    
 
-        //run the programmable block with the state name as a parameter 
+        //run the programmable block with the state name as a parameter    
 
-        /*
-        idle loop:
-         time(idle)
-         list all named blocks(blocks)
-         list all registered states(ls)
-
+        /*   
+        idle loop:   
+         time(idle)   
+         list all named blocks(blocks)   
+         list all registered states(ls)   
+   
         */
+
 
         const string TIMER_NAME = "STATE TIMER";
         const string LCD_NAME = "STATE LCD";
         const string IDLE_STATE_NAME = "IDLE";
-     
 
-        // what an opportunity to refactor the code ..
+
+
+
+        // what an opportunity to refactor the code ..   
 
         readonly BlockAction TimeAction = new BlockAction(
             LCD_NAME, lcd => (lcd as IMyTextPanel)
             ?.WritePublicText(" " + DateTime.UtcNow.ToLongTimeString()));
 
-        BlockAction CheckTarget(string name)
-        {
-            Action<IMyTerminalBlock> action = block =>
-            {
-                var lcd_block = this.GridTerminalSystem.GetBlockWithName(LCD_NAME);
-                var lcd = (lcd_block as IMyTextPanel);
-                lcd?.WritePublicText(" '" + name + "' FOUND:\n" + block.DetailedInfo, append: true);
-            };
-            return new BlockAction(name, action);
-        }
-
-
-        // what an opportunity to refactor the code ..
+        // what an opportunity to refactor the code ..   
         readonly BlockAction ClearLCDAction = new BlockAction(
            LCD_NAME, lcd => (lcd as IMyTextPanel)
            ?.WritePublicText("", append: false));
 
-        List<BlockState> states;
+        List<NamedState> states;
 
         public Program()
-        {            
-            states = new List<BlockState>();
-            //state entry points
-            var idle = new BlockState(IDLE_STATE_NAME, null, 5.0);
+        {
+            states = new List<NamedState>();
+            //state entry points   
+            var idle = new NamedState(IDLE_STATE_NAME, ClearLCDAction, 5.0);
 
-            //idle loop
+
+
+            //idle loop   
             states.SetUpSequence(new[] {
             idle,
-                new BlockState("time", TimeAction, 5.0),
-                new BlockState( "blocks", LCD_NAME,
-                lcd => {
+                new NamedState("time", TimeAction, 5.0),
+                new NamedState( "blocks", LCD_NAME,
+                action:lcd => {
                     (lcd as IMyTextPanel)?.WritePublicText("");
-                    var statenames = states.Select(s => s.BlockAction?.BlockName)
+                    var statenames = states
+                        .Where(s => s.NamedAction  is BlockAction )
+                        .Select(s => s.NamedAction?.Name)
                         .Concat(new[] { TIMER_NAME })
                         .Distinct();
                     foreach (var name in statenames)
@@ -77,14 +72,30 @@ namespace IdleLoop
                         (lcd as IMyTextPanel)
                             ?.WritePublicText("'" + name + "'" + found_text, true);
                     }},
-                5.0),
+                delay:5.0),
+                new NamedState( "groups", LCD_NAME,
+                action:lcd => {
+                    (lcd as IMyTextPanel)?.WritePublicText("");
+                    var statenames = states
+                        .Where(s => s.NamedAction  is GroupAction )
+                        .Select(s => s.NamedAction?.Name)
+                        .Distinct();
+                    foreach (var name in statenames)
+                    {
+                        var found_text =
+                            this.GridTerminalSystem.GetBlockGroupWithName(name) == null ? "\n" : " FOUND\n";
+                        (lcd as IMyTextPanel) ?.WritePublicText("'" + name + "'" + found_text, true);
+                    }},
+                delay:5.0),
 
-            new BlockState( "ls", LCD_NAME,
-                lcd => (lcd as IMyTextPanel)?.WritePublicText(
+            new NamedState( "ls", LCD_NAME,
+                action:lcd => (lcd as IMyTextPanel)?.WritePublicText(
                         string.Join(",\n", states.Select(s => s.to_str())) + "\n"),
-                5.0),
+                delay:5.0),
             idle});
         }
+
+
 
         public void Main(string eventName)
         {
@@ -99,7 +110,7 @@ namespace IdleLoop
             states.ForEach(state => StartTimer(state, eventName));
         }
 
-        public void StartTimer(BlockState state, string name = null)
+        public void StartTimer(NamedState state, string name = null)
         {
             if (state == null || name != null && state.Name != name) return;
             SaveString(state.Name);
@@ -110,12 +121,12 @@ namespace IdleLoop
             timer.ApplyAction("Start");
         }
 
-        public void Execute(BlockState state, string name)
+        public void Execute(NamedState state, string name)
         {
             if (state == null || state.Name != name) return;
             var lcd = GridTerminalSystem.GetBlockWithName(LCD_NAME) as IMyTextPanel;
             lcd?.WritePublicText(">" + state.Name, true);
-            state.BlockAction?.Invoke(this);
+            state.NamedAction?.Invoke(this);
             StartTimer(state.Next);
         }
 
@@ -135,59 +146,90 @@ namespace IdleLoop
         }
     }
 
-    public class BlockAction
+    public class GroupAction : NamedAction
     {
-        public string BlockName { get; }
-        private Action<IMyTerminalBlock> _action { get; }
-        public void Invoke(MyGridProgram env) => env
+        override public string Name { get; }
+        private Action<List<IMyTerminalBlock>> _action { get; }
+        override public void Invoke(MyGridProgram env) => env
             .GridTerminalSystem
-            .GetBlockWithName(BlockName)
+            .GetBlockGroupWithName(Name)
+            ?.Apply(_action);
+
+        public GroupAction(
+            string group_name,
+            Action<List<IMyTerminalBlock>> action)
+        {
+            Name = group_name;
+            _action = action;
+        }
+    }
+
+
+    public class BlockAction : NamedAction
+    {
+        override public string Name { get; }
+        private Action<IMyTerminalBlock> _action { get; }
+        override public void Invoke(MyGridProgram env) => env
+            .GridTerminalSystem
+            .GetBlockWithName(Name)
             ?.Apply(_action);
 
         public BlockAction(
             string block_name,
             Action<IMyTerminalBlock> action)
         {
-            BlockName = block_name;
+            Name = block_name;
             _action = action;
         }
     }
-    public class ActionBase
-    {
 
+    public abstract class NamedAction
+    {
+        abstract public string Name { get; }
+        abstract public void Invoke(MyGridProgram env);
     }
 
-    public class BlockState
+
+
+    public class NamedState
     {
-        public BlockAction BlockAction { get; }
+        public NamedAction NamedAction { get; }
         public double Delay { get; }
-        public BlockState Next { get; set; }
+        public NamedState Next { get; set; }
         public string Name { get; }
 
-        public BlockState(
+        public NamedState(
             string name,
             string block_name,
             Action<IMyTerminalBlock> action,
             double delay,
-            BlockState next = null) :
+            NamedState next = null) :
             this(name, new BlockAction(block_name, action), delay, next)
         { }
 
-
-        public BlockState(
+        public NamedState(
             string name,
-            BlockAction block_action,
+            string block_name,
+            Action<List<IMyTerminalBlock>> group_action,
             double delay,
-            BlockState next = null)
+            NamedState next = null) :
+            this(name, new GroupAction(block_name, group_action), delay, next)
+        { }
+
+        public NamedState(
+            string name,
+            NamedAction block_action,
+            double delay,
+            NamedState next = null)
         {
-            BlockAction = block_action;
+            NamedAction = block_action;
             Delay = delay;
             Name = name;
             Next = next;
         }
 
         public string to_str() =>
-            "'" + Name + "':'" + BlockAction?.BlockName + "'" +
+            "'" + Name + "':'" + NamedAction?.Name + "'" +
             (Next != null ? (" -> " + "'" + Next.Name + "'") : "");
     }
 
@@ -196,10 +238,19 @@ namespace IdleLoop
         public static void Apply(this IMyTerminalBlock block, Action<IMyTerminalBlock> action)
             => action(block);
 
-        public static List<BlockState> SetUpSequence(this List<BlockState> states,
-            IEnumerable<BlockState> registered_states)
+        public static void Apply(this IMyBlockGroup group, Action<List<IMyTerminalBlock>> action)
         {
-            BlockState last = null;
+            var blocks = new List<IMyTerminalBlock>();
+            group.GetBlocks(blocks);
+
+            action(blocks);
+        }
+
+
+        public static List<NamedState> SetUpSequence(this List<NamedState> states,
+            IEnumerable<NamedState> registered_states)
+        {
+            NamedState last = null;
             foreach (var state in registered_states.Reverse())
             {
                 if (last != null)
